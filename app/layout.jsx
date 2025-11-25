071bc282149ff81ac16066ba57c242e0e0b4e1f5
karim ossama <karimossama591@gmail.com>
2025-11-25 22:16:49 +0100

    edit store

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,7 +6,7 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "MiracShop",
+  title: "DivanStore",
   manifest: '/manifest.json',
 };
 
